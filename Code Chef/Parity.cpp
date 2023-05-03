e2a4fef7a67f01f20e797e1a9fbd8940677b9bac
rudraaatt <122170344+rudraaatt@users.noreply.github.com>
2023-05-04 00:11:33 +0530

    Create Parity.cpp

---
+++ Code Chef/Parity.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+    int N;
+    cin >> N;
+    
+    if(N % 2==0){
+    cout << "Yes" << endl;
+}
+    else{
+    cout << "No" << endl;
+    }
+    }
+	
+	return 0;
+}
