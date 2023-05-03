5c61a3279b15a78060a07a5f852eacd35367f558
rudraaatt <122170344+rudraaatt@users.noreply.github.com>
2023-05-03 14:47:54 +0530

    Create Age Limit.cpp

---
+++ Code Chef/Age Limit.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+	cin >> t;
+	while (t--){
+    int Age,X,Y;
+	cin >> X >> Y >> Age;
+	
+	if ( Age>=X && Age<Y ){
+	 cout << "YES" << endl;
+	}
+	else {
+	 cout << "NO" << endl ;
+}
+	}
+}
