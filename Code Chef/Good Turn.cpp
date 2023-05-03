dd467b190237a2b146fbe0b16ffc12363400ac90
rudraaatt <122170344+rudraaatt@users.noreply.github.com>
2023-05-03 22:13:58 +0530

    Create Good Turn.cpp

---
+++ Code Chef/Good Turn.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--){
+	    
+	 int x , y;
+	    cin >> x >> y;
+	    int sum = x+y;
+	    
+	    
+	 
+	if (sum >6){
+	        cout << "YES" << endl ;
+}
+    else {
+      cout << "NO" << endl ;
+}
+	    
+	}
+	return 0;
+}
